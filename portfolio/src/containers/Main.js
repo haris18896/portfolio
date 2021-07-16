14f381c874078283539e8404364666fb43b1dd78
Haris Ahmad <haris18896@gmail.com>
2021-07-16 00:13:05 -0700

    starting now

---
+++ portfolio/src/containers/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Main.css';
+
+function Main() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+        </div>
+    )
+}
+
+export default Main
---
+++ portfolio/src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAWw9sXgn2kzUqOHwtOHoVmzDzPHaRwqkU",
+    authDomain: "portfolio-39c92.firebaseapp.com",
+    projectId: "portfolio-39c92",
+    storageBucket: "portfolio-39c92.appspot.com",
+    messagingSenderId: "143229469455",
+    appId: "1:143229469455:web:1640119036c1073599347a",
+    measurementId: "G-1QMRY9NFDM"
+  };
+
+  firebase.initializeApp(firebaseConfig);
