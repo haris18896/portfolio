89c6e7b2dbd9461efe85346b7536892969028f8a
Haris Ahmad <haris18896@gmail.com>
2021-07-16 00:37:48 -0700

    portfolio.js added

---
+++ portfolio/src/portfolio.js
@@ -0,0 +1,22 @@
+import emoji from 'react-easy-emoji';
+
+const illustration = {
+    animated : true,
+};
+
+const greeting = {
+    username: "Haris Ahmad Khan",
+    title: "Hi all, I'm Haris",
+    subTitle: emoji( "I'm an Electronic Engineer and A passionate Full Stack Web App Developer 🚀 having an experience of designing and building Web applications with JavaScript / React-js / Python and some other cool libraries and frameworks."),
+    resumeLink: "",
+    displayGreeting: true,
+
+};
+
+
+
+
+export {
+    illustration,
+    greeting,
+};
